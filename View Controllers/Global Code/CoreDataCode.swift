550b48bb0eddbce3f1b894b8430c3430c4990a57
Lance Owide <lanceowide@gmail.com>
2020-01-30 17:49:34 -0600

    CoreData and associated firebase data retrieval complete

--- View Controllers/Global Code/CoreDataCode.swift
+++ View Controllers/Global Code/CoreDataCode.swift
@@ -20,11 +20,10 @@ class CoreDataCode: UIViewController {
 
 extension UIViewController{
     
-    
+//    first function we call when the app opens, this itterates through all other functions
     func CDAppHasLoaded(){
-     
 //        fetch the core data currently saved, returns true if we didn't recieve an error
-        if CDFetchEventData() == true{
+        if CDFetchEventDataFromDB() == true{
             print("we contacted CoreData")
          
             if CDEevents.count == 0{
@@ -35,29 +34,27 @@ extension UIViewController{
             else{
                 print("there were events in CoreData")
 //             if events were returned, we only want to retrieve the events for which we have a notification
-                
-                
-                
-            }
-            
-            
-        }
+                CDRetrieveUpdatedEventCheck{(eventIDs) in
+                    
+                    self.CDRetrieveUpdatedEvents(eventIDs: eventIDs)
+                    
+                    }}}
         else{
             print("there was an issue contacting core data")
-        }
-        
-        
-    }
+        }}
     
     
 //    check for events with updated flags in the userEventsUpdate table
-    func CDRetrieveUpdatedEventCheck() -> [String]{
+    func CDRetrieveUpdatedEventCheck(completion: @escaping (_ eventIDString: [String:Any]) -> Void){
         
-        let eventIDString = [String]()
+        print("running func CDRetrieveUpdatedEventCheck")
+        
+        var eventIDString = [String: Any]()
         
         dbStore.collection("userEventUpdates").document(user!).getDocument{ (querySnapshot, error) in
                 if error != nil {
                     print("Error getting documents: \(error!)")
+                    completion(eventIDString)
                 }
                 else {
                     
@@ -65,24 +62,131 @@ extension UIViewController{
                       
         //                the user doesn't have any  event data to retrieve
                         print("user has no new event notifications")
+                        completion(eventIDString)}
+                    else{
+                        print("user has new event notifications")
                         
+                        let documentData: [String: Any]  = (querySnapshot?.data())!
+                        
+                        eventIDString = documentData
+                        
+                        print("documentData \(documentData)")
+                        completion(eventIDString)
+                }}}}
+    
+    
+//    adds, deletes and amends events based on the userEventNotifications table in FireStore
+    func CDRetrieveUpdatedEvents(eventIDs: [String: Any]){
+     
+        print("running func CDRetrieveUpdatedEvents inputs - eventIDs: \(eventIDs)")
+        
+//        loop thorugh the eventIDs and determine the action for each notification
+        for i in eventIDs{
+            let notification = i.value as! String
+            
+            if notification == "delete"{
+                print("CDRetrieveUpdatedEvents - deleting event \(i.key)")
+//                event has been deleted, we remove it from our array of events
+                if let index = CDEevents.index(where: {$0.eventID == i.key}){
+                    context.delete(CDEevents[index])
+                    CDEevents.remove(at: index)
+                    print("index: \(index)")
+                    self.CDSaveData()
+                }
+            }
+            else if notification == "amend"{
+                print("CDRetrieveUpdatedEvents - amending event \(i.key)")
+//                event has been updated, delete the event from our array of events and repalce it
+                if let index = CDEevents.index(where: {$0.eventID == i.key}){
+                    context.delete(CDEevents[index])
+                    CDEevents.remove(at: index)
+                    print("index: \(index)")
+                    self.CDSaveData()
+                }
+                CDRetrieveSinglEventsFB(eventID: i.key)
+            }
+            else if notification == "new"{
+                print("CDRetrieveUpdatedEvents - new event \(i.key)")
+                if let index = CDEevents.index(where: {$0.eventID == i.key}){
+                    context.delete(CDEevents[index])
+                    CDEevents.remove(at: index)
+                    print("index: \(index)")
+                    self.CDSaveData()
+                }
+                CDRetrieveSinglEventsFB(eventID: i.key)
+            }}
+//        once all notifications have been looped through, they are deleted from Firebase
+        removeTheEventNotifications()
+    }
+    
+    //    function to retrieve single event from Firebase
+    func CDRetrieveSinglEventsFB(eventID: String){
+            print("running func CDRetrieveSinglEventsFB input - eventID: \(eventID)")
+        dbStore.collection("eventRequests").document(eventID).getDocument{ (querySnapshot, error) in
+                if error != nil {
+                    print("Error getting documents: \(error!)")
+                }
+                else {
+                    if querySnapshot!.exists == false{
+                      
+        //                the user doesn't have any  event data to retrieve
+                        print("the event is no longer available")
                     }
                     else{
+                         let eventID = querySnapshot!.documentID
+                         let startTimeString = querySnapshot!.get("startTimeInput") as! String
+                         let startDateString = querySnapshot!.get("startDateInput") as! String
+                         let allStartDates = querySnapshot!.get("startDates") as! [String]
+                         let endTimeString = querySnapshot!.get("endTimeInput") as! String
+                         let allEndDates = querySnapshot!.get("endDates") as! [String]
+                         let endDateString = querySnapshot!.get("endDateInput") as! String
+                         let daysOfTheWeek = querySnapshot!.get("daysOfTheWeek") as! [Int]
+                         let chosenDatePosition = querySnapshot!.get("chosenDatePosition") as? Int ?? 999
+                         let chosenDateCD = querySnapshot!.get("chosenDate") as? String ?? ""
+                         let chosenDateDay = querySnapshot!.get("chosenDateDay") as? Int ?? 999
+                         let secondsFromGMT = querySnapshot!.get("secondsFromGMT") as? Int ?? 999
+                         let chosenDateMonth = querySnapshot!.get("chosenDateMonth") as? Int ?? 999
+                         let chosenDateYear = querySnapshot!.get("chosenDateYear") as? Int ?? 999
+                         let eventCreatorName = querySnapshot!.get("eventOwnerName") as? String ?? ""
+                         let eventCreatorID = querySnapshot!.get("eventOwner") as? String ?? ""
+                         let eventLocation = querySnapshot!.get("location") as? String ?? ""
+                         let isAllDay = querySnapshot!.get("isAllDay") as? String ?? ""
+                         let eventDescription = querySnapshot!.get("eventDescription") as? String ?? ""
+                         let locationLongitude = querySnapshot!.get("locationLongitude") as? Double ?? 0.0
+                         let locationLatitude = querySnapshot!.get("locationLatitude") as? Double ?? 0.0
+                         
+                         let CDNewEvent = CoreDataEvent(context: context)
+                         
+                         CDNewEvent.chosenDate = chosenDateCD
+                         CDNewEvent.chosenDateDay = Int32(chosenDateDay)
+                         CDNewEvent.chosenDateMonth = Int32(chosenDateMonth)
+                         CDNewEvent.chosenDatePosition = Int32(chosenDatePosition)
+                         CDNewEvent.chosenDateYear = Int32(chosenDateYear)
+                         CDNewEvent.daysOfTheWeek = daysOfTheWeek
+                         CDNewEvent.endDateInput = endDateString
+                         CDNewEvent.endDates = allEndDates
+                         CDNewEvent.endTimeInput = endTimeString
+                         CDNewEvent.eventDescription = eventDescription
+                         CDNewEvent.eventID = eventID
+                         CDNewEvent.eventOwner = eventCreatorID
+                         CDNewEvent.eventOwnerName = eventCreatorName
+                         CDNewEvent.isAllDay = isAllDay
+                         CDNewEvent.location = eventLocation
+                         CDNewEvent.locationLatitue = locationLatitude
+                         CDNewEvent.locationLongitude = locationLongitude
+                         CDNewEvent.secondsFromGMT = Int32(secondsFromGMT)
+                         CDNewEvent.startDates = allStartDates
+                         CDNewEvent.startDateInput = startDateString
+                         CDNewEvent.startTimeInput = startTimeString
+                         
+ //                        append the new event onto CDNewEvent
+                         CDEevents.append(CDNewEvent)
+                        self.CDSaveData()
                     }
+//                    print("CDEevents \(CDEevents)")
+                    
             }
-        }
-        
-            
-      return eventIDString
-    }
-    
-    
-    
-    func CDRetrieveUpdatedEvents(){
-        
-        
-        
-    }
+        }}
     
     
 //    function to retrieve all the events from Firebase the user has been invited to, this should only be used if the coredata is currently empty
@@ -157,13 +261,13 @@ extension UIViewController{
                         
     
                     }
-                    print("CDEevents \(CDEevents)")
+//                    print("CDEevents \(CDEevents)")
                     self.CDSaveData()
                 }
                    }}}
     
     
-    func CDFetchEventData() -> Bool{
+    func CDFetchEventDataFromDB() -> Bool{
         
         let request : NSFetchRequest<CoreDataEvent> = CoreDataEvent.fetchRequest()
         
@@ -185,14 +289,30 @@ extension UIViewController{
     
 //    function save down the core data
     func CDSaveData(){
+        print("running func CDSaveData")
         
         do{
         try context.save()
-
         }
         catch{
             print("error saving data to core data with error \(error)")
         }
+        
+    }
+    
+    
+    func removeTheEventNotifications(){
+        
+        print("running func - removeTheEventNotifications")
+        
+        dbStore.collection("userEventUpdates").document(user!).delete() { err in
+            if let err = err {
+                print("Error removing document: \(err)")
+            } else {
+                print("Document successfully removed!")
+            }
+        }
+        
         
     }
     
