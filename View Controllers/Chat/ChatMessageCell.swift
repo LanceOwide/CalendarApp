0a14880db3d40693d9760816f8877f9a07dcfc16
Lance Owide <lance@planr.me>
2020-12-01 22:35:35 -0600

    links within chat are now recognised

--- View Controllers/Chat/ChatMessageCell.swift
+++ View Controllers/Chat/ChatMessageCell.swift
@@ -14,6 +14,7 @@ class ChatMessageCell: UICollectionViewCell {
     let textView: UITextView = {
         let tv = UITextView()
         tv.text = "SAMPLE TEXT FOR NOW"
+        tv.dataDetectorTypes = .all
         tv.font = UIFont.systemFont(ofSize: 16)
         tv.translatesAutoresizingMaskIntoConstraints = false
         tv.backgroundColor = UIColor.clear
